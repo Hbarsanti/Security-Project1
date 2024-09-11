cc6ee7d19779807dc3395bfeeece8f451a4104de
Henry Barsanti <henrybarsanti@Henrys-MacBook-Pro.local>
2024-09-11 15:15:23 -0500

    getting input

--- test.cpp
+++ test.cpp
@@ -1,18 +1,28 @@
 #include <fstream> 
 #include <iostream> 
 #include <string> 
+#include <sstream>
 using namespace std;
 
 int main() {
   ifstream inputFile("ciphertext.txt");
 
+  stringstream buffer;
   string line;
+  string cipherText;
+
+  buffer << inputFile.rdbuf();
+
+  cipherText = buffer.str();
   
-  cout << "File Content: " << endl; 
-    while (getline(inputFile, line)) { 
-        cout << line << endl; 
-    } 
+  //cout << "File Content: " << endl; 
+   // while (getline(inputFile, line)) { 
+     //   cout << line << endl; 
+   // } 
+  cout << cipherText << endl;
   
+
+
     
     inputFile.close(); 
   return 0;
