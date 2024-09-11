718c31402f50d7916ae88ecbff390711535e8a89
Henry Barsanti <henrybarsanti@Henrys-MacBook-Pro.local>
2024-09-11 15:39:24 -0500

    reading file

--- test.cpp
+++ test.cpp
@@ -4,43 +4,33 @@
 #include <sstream>
 using namespace std;
 
-int main() {
-  ifstream inputFile("ciphertext.txt");
+// read ciphertext and dictionary
+
+string readInput(string fileName){
+  ifstream inputFile(fileName);
 
   stringstream buffer;
-  string line;
-  string cipherText;
+  string inputText;
 
   buffer << inputFile.rdbuf();
 
-  cipherText = buffer.str();
+  inputText = buffer.str();
   
-  //cout << "File Content: " << endl; 
-   // while (getline(inputFile, line)) { 
-     //   cout << line << endl; 
-   // } 
-  cout << cipherText << endl;
   
+  //cout << inputText << endl;
+  inputFile.close(); 
+  return inputText;
 
-
-    
-    inputFile.close(); 
-  return 0;
-
-  // read ciphertext and dictionary
-std::string readFile(const std::string &filename) {
-    std::ifstream file(filename);
-    std::string text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    return text;
 }
+
 // compute frequency of each letter in ciphertext and sort them
-std::map<char, int> frequency(const std::string &text) {
+/*std::map<char, int> frequency(const std::string &text) {
     std::map<char, int> freq;
     
         }
     }
     return freq;
-}
+}*/
 // map the sorted frequency of ciphertext letters to the standard English letter frequency
 
 // decrypt the ciphertext
@@ -48,7 +38,18 @@ std::map<char, int> frequency(const std::string &text) {
 // Count the number of dictionary words in the decrypted text
 
 // Improve the key by permutation
+
+int main() {
+
+  readInput("ciphertext.txt");
+
+  return 0;
 }
+
+
+
+
+
 
 
 
